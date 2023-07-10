5aaad260e1e47ab89e93c632ce11a5844833bc20
Patty Rose <oteropatty@gmail.com>
2023-07-10 13:30:34 -0700

    BE: add initial migration

--- intsureview_be/intsureview_be/apps/api/serializers.py
+++ intsureview_be/intsureview_be/apps/api/serializers.py
@@ -17,3 +17,13 @@ class TestRecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = TestRecipe
         fields = ["recipe_name", "make_again", "recipe", "rating", "notes"]
+
+    def validate_rating(self, value):
+        if not 0 <= value <= 10:
+            raise serializers.ValidationError("Rating should be between 0 and 10.")
+        return value
+      
+    def validate_make_again(self, value):
+        if value.lower() not in ["yes", "no"]:
+            raise serializers.ValidationError("Invalid value for make_again. Must be either 'Yes' or 'No'.")
+        return value
