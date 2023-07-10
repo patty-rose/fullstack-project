54a5c8e8354ae901ea4a0ce190d2eb11c9eeecd0
Patty Rose <oteropatty@gmail.com>
2023-07-10 11:17:49 -0700

    FE: add button and form tags to complete CentralContent form

--- intsureview_fe/src/components/CentralContent.js
+++ intsureview_fe/src/components/CentralContent.js
@@ -3,6 +3,7 @@ import InputField from "./InputField";
 import FormTitle from "./FormTitle";
 import TextAreaField from "./TextAreaField";
 import SelectField from "./SelectField";
+import Button from "./Button";
 
 const CentralContent = () => {
   const [recipeName, setRecipeName] = useState("");
@@ -12,61 +13,53 @@ const CentralContent = () => {
   const [notes, setNotes] = useState("");
   return (
     <main data-testid="central-content">
-      {/* 
-      this will contain:
-      -title
-      -form
-      -input for recipe name
-      -select input for make again
-      -text area for recipe
-      -number input for rating
-      -text area for notes
-      -submit button
-      */}
       <FormTitle text="Add a Test Recipe:" />
-      <InputField
-        label="Recipe Name"
-        id="recipeName"
-        value={recipeName}
-        onChange={(e) => setRecipeName(e.target.value)}
-        required
-      />
+      <form>
+        <InputField
+          label="Recipe Name"
+          id="recipeName"
+          value={recipeName}
+          onChange={(e) => setRecipeName(e.target.value)}
+          required
+        />
 
-      <SelectField
-        label="Would you make this again?"
-        id="makeAgain"
-        value={makeAgain}
-        onChange={(e) => setMakeAgain(e.target.value)}
-        required
-        options={["Yes", "No"]}
-      />
+        <SelectField
+          label="Would you make this again?"
+          id="makeAgain"
+          value={makeAgain}
+          onChange={(e) => setMakeAgain(e.target.value)}
+          required
+          options={["Yes", "No"]}
+        />
 
-      <TextAreaField
-        label="Recipe"
-        id="recipe"
-        value={recipe}
-        onChange={(e) => setRecipe(e.target.value)}
-        required
-      />
+        <TextAreaField
+          label="Recipe"
+          id="recipe"
+          value={recipe}
+          onChange={(e) => setRecipe(e.target.value)}
+          required
+        />
 
-      <InputField
-        label="Rating 0-10"
-        id="rating"
-        value={rating}
-        onChange={(e) => setRating(e.target.value)}
-        required
-        type="number"
-        min={0}
-        max={10}
-      />
+        <InputField
+          label="Rating 0-10"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+          required
+          type="number"
+          min={0}
+          max={10}
+        />
 
-      <TextAreaField
-        label="Notes"
-        id="notes"
-        value={notes}
-        onChange={(e) => setNotes(e.target.value)}
-        required
-      />
+        <TextAreaField
+          label="Notes"
+          id="notes"
+          value={notes}
+          onChange={(e) => setNotes(e.target.value)}
+          required
+        />
+        <Button type="submit">Submit Test Recipe</Button>
+      </form>
     </main>
   );
 };
