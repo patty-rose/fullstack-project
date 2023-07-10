e75b4ef78716053620df12fee2aceab010ee2c63
Patty Rose <oteropatty@gmail.com>
2023-07-10 14:16:20 -0700

    FE: add minimal styling and comment

--- intsureview_fe/src/App.js
+++ intsureview_fe/src/App.js
@@ -4,6 +4,8 @@ import CentralContent from './components/CentralContent';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+//very minimal css styling added as it was not in the project instructions
+
 function App() {
   return (
     <div className="App">
