e175d5b5d17467a9ab241ea073677effa9fa708a
Patty Rose <oteropatty@gmail.com>
2023-07-10 12:59:32 -0700

    FE: add CentralContent form validation tests

--- intsureview_fe/src/tests/components/CentralContent.test.js
+++ intsureview_fe/src/tests/components/CentralContent.test.js
@@ -9,14 +9,61 @@ describe("CentralContent", () => {
     expect(headingElement).toBeInTheDocument();
   });
 
-  test('renders form with fields and button', () => {
+  test("renders form with fields and button", () => {
     render(<CentralContent />);
 
-    expect(screen.getByLabelText('Recipe Name')).toBeInTheDocument();
-    expect(screen.getByLabelText('Would you make this again?')).toBeInTheDocument();
-    expect(screen.getByLabelText('Recipe')).toBeInTheDocument();
-    expect(screen.getByLabelText('Rating 0-10')).toBeInTheDocument();
-    expect(screen.getByLabelText('Notes')).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: 'Submit Test Recipe' })).toBeInTheDocument();
+    expect(screen.getByLabelText("Recipe Name")).toBeInTheDocument();
+    expect(
+      screen.getByLabelText("Would you make this again?")
+    ).toBeInTheDocument();
+    expect(screen.getByLabelText("Recipe")).toBeInTheDocument();
+    expect(screen.getByLabelText("Rating 0-10")).toBeInTheDocument();
+    expect(screen.getByLabelText("Notes")).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "Submit Test Recipe" })
+    ).toBeInTheDocument();
+  });
+
+  test("displays error messages for invalid form submission", () => {
+    render(<CentralContent />);
+
+    const submitButton = screen.getByRole("button", {
+      name: "Submit Test Recipe",
+    });
+    fireEvent.click(submitButton);
+
+    expect(screen.getByText("Recipe Name is required")).toBeInTheDocument();
+    expect(screen.getByText("Selection required")).toBeInTheDocument();
+    expect(screen.getByText("Recipe is required")).toBeInTheDocument();
+    expect(screen.getByText("Rating is required")).toBeInTheDocument();
+    expect(screen.getByText("Notes is required")).toBeInTheDocument();
+  });
+
+  test("displays success message for valid form submission", () => {
+    render(<CentralContent />);
+
+    const submitButton = screen.getByRole("button", {
+      name: "Submit Test Recipe",
+    });
+    fireEvent.change(screen.getByLabelText("Recipe Name"), {
+      target: { value: "Test Recipe" },
+    });
+    fireEvent.change(screen.getByLabelText("Would you make this again?"), {
+      target: { value: "Yes" },
+    });
+    fireEvent.change(screen.getByLabelText("Recipe"), {
+      target: { value: "test recipe content" },
+    });
+    fireEvent.change(screen.getByLabelText("Rating 0-10"), {
+      target: { value: "7" },
+    });
+    fireEvent.change(screen.getByLabelText("Notes"), {
+      target: { value: "test notes" },
+    });
+    fireEvent.click(submitButton);
+
+    expect(
+      screen.getByText("Form submitted successfully! You may submit another.")
+    ).toBeInTheDocument();
   });
 });
