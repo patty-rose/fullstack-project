9997954cb77dea9f1676fd7b0a455b3aea753454
Patty Rose <oteropatty@gmail.com>
2023-07-10 13:30:01 -0700

    BE: add test-recipes serializer, view, and route

--- intsureview_be/intsureview_be/apps/api/serializers.py
+++ intsureview_be/intsureview_be/apps/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-
+from intsureview_be.apps.api.models import TestRecipe
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -12,3 +12,8 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ["url", "name"]
+
+class TestRecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TestRecipe
+        fields = ["recipe_name", "make_again", "recipe", "rating", "notes"]
